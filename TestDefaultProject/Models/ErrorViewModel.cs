898ba761ce5f7deeabe655368a65ce61923fc5cc
YC_Lin <t871005y@gmail.com>
2021-02-21 22:04:59 +0800

    first commit

---
+++ TestDefaultProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestDefaultProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
